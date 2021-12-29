396e42be2f9ca2af631bb589b1a8d0d0aa518fa5
Kate Brender <kate.brender@gmail.com>
2021-12-28 21:58:49 -0500

    Initial commit

---
+++ process_grades.py
@@ -0,0 +1,6 @@
+# Open the rubric csv and establish a bunch of variables
+
+# Open the scores csv, iterate through the rows:
+	# create a template context for the student
+	# fill in the html with the context
+	# process the html into a pdf, name it correctly
